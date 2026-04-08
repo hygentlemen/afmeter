3161e8d518a28bf2bbef7457a4815b454eedffbc
scott qi <scott@scottdeMac-mini.local>
2026-04-08 10:26:01 +0800

    Fix build issues for deployment

--- i18n-config.ts
+++ i18n-config.ts
@@ -1,4 +1,6 @@
-{
-  "locales": ["en", "zh"] as const,
-  "defaultLocale": "en"
-}
+export const i18nConfig = {
+  locales: ["en", "zh"] as const,
+  defaultLocale: "en"
+} as const;
+
+export type Locale = typeof i18nConfig.locales[number];
