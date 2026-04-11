d5f9bcecd7d25a58ffd0a686da4289f722bf1d2d
scott qi <scott@scottdeMac-mini.local>
2026-04-11 08:38:07 +0800

    Explicitly add favicon link tags to HTML head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,6 +39,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/icon-192.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
